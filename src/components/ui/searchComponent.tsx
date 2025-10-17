2e819f455a33e0d2ad420e2a57f511efcf084410
Choijiin007 <cji2025@naver.com>
2025-10-17 16:46:35 +0900

    design(search): 검색 input에 css 추가

--- src/components/ui/searchComponent.tsx
+++ src/components/ui/searchComponent.tsx
@@ -1,17 +1,20 @@
 import { Search } from 'lucide-react'
+import { useEffect } from 'react'
 export default function SearchComponent() {
   return (
     <>
-      <div className="flex flex-col">
-        <div>지금 마음, 익명으로 털어놓아도 괜찮아요.</div>
-        <div className="flex gap-2 items-center border-1 border-sky-500">
-          <Search className="mr-2 h-4 w-4 shrink-0 opacity-50 border-1 border-purple-500" />
+      <div className="flex flex-col max-w-4xl mx-auto bg-zinc-900">
+        <p className="text-sm text-slate-50 mb-4">지금 마음, 익명으로 털어놓아도 괜찮아요.</p>
+        <div className="flex gap-2 items-center outline outline-slate-50 p-2 has-[:focus]:outline-2 has-[:focus]:outline-sky-400 rounded-xl mb-4">
+          <Search className="mr-2 h-4 w-4 shrink-0 opacity-50 text-slate-50" />
           <input
             type="text"
-            className="border-1 border-sky-500"
+            className="flex-1 text-slate-50 outline-none"
             placeholder="내용이나 태그로 검색..."
           ></input>
         </div>
+        {/* 선택true - bg: sky-400, text:zinc-900 */}
+        {/* 선택 false - bg:gray-700/50. text: slate-400,  hover: gray-700*/}
         <div>
           <ul>
             <li>전체</li>
